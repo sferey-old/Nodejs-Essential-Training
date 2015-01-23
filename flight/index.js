334396f0cfd13ea613f8333fbf19bf7fb07c7a64
marley-nodejs <marley.nodejs@gmail.com>
2015-01-23 22:47:22 +0000

    # 04. Модули | 04-3. Реализация шаблонов проектирования | Factory

--- flight/index.js
+++ flight/index.js
@@ -1,6 +1,6 @@
-module.exports = function (info) {
+var Flight = function(){
 
-	var values = {
+	this.data = {
 		number: null,
 		origin: null,
 		destination: null,
@@ -10,24 +10,34 @@ module.exports = function (info) {
 		actualArrive: null
 	};
 
-	for(var prop in values) {
-		if(values[prop] !== 'undefined') {
-			values[prop] = info[prop];
-		}
-	}
-
-	var functions = {
-		triggerDepart: function () {
-			values.actualDepart = Date.now();
-		},
-		triggerArrive: function () {
-			values.actualArrive = Date.now();
-		},
-		getInformation: function () {
-			return values;
+	this.fill = function(info){
+		for(var prop in this.data) {
+			if(this.data[prop] !== 'undefined') {
+				this.data[prop] = info[prop];
+			}
 		}
 	};
 
-	return functions;
 
+	this.triggerDepart = function () {
+		this.data.actualDepart = Date.now();
+	};
+
+	this.triggerArrive = function () {
+		this.data.actualArrive = Date.now();
+	};
+
+	this.getInformation = function () {
+		return this.data;
+	};
+
+};
+
+
+module.exports = function (info) {
+
+	var instance = new Flight();
+
+	instance.fill(info);
+	return instance;
 };
